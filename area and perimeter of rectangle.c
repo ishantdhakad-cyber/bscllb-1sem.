cd21fbe6110665d78230210a99384b56c3094be9
ishantdhakad-cyber <ishant13dhakad@gmail.com>
2025-08-28 12:09:12 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int width=4;
+    int Length=6;
+    int area=Length*width;
+    int perimeter=2*(Length+width);
+    
+    printf("width :%d\n",width);
+    printf("Length :%d\n",Length);
+    printf("area of rectangle :%d\n",area);
+    printf("perimeter of rectangle :%d\n",perimeter);
+}
